5919856542f8e37b9765227652e6f0b6dd6410e2
A-Souhei <sdritoavina@yahoo.fr>
2017-03-24 07:07:11 +0300

    Functions are Ok

--- 2_calculator.py
+++ 2_calculator.py
@@ -9,6 +9,8 @@ Exercise:
 5. Add multiplication and division (requires writing new function)
 
 """
+
+
 def add_two_numbers(x,y):
     """ Adds two numbers """
     return float(x) + float(y)
@@ -21,8 +23,9 @@ def subtract_two_numbers(x,y):
 print("Hello! This is a simple program for simple calculations")
 
 #Specify operation
-print("What would you like to do today?")
-doToday = "add" #HACK
+print('\x1b[1;31m'+"What would you like to do today?"+'\x1b[0m')
+#doToday = "add" #HACK
+doToday = input("add for addition \nsubs for substraction\noperation:")
 print("We will " + doToday + " two numbers now.")
 
 var1 = input("First number: ")
@@ -30,6 +33,8 @@ var2 = input("Second number: ")
 
 if doToday=="add":
     result = add_two_numbers(var1, var2)
+else:
+	result = subtract_two_numbers(var1 , var2)
 
 print("This is the result:", result)
 print("I hope it is correct...")
